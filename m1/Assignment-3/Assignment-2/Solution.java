d7398a01cfe37071e817e91dae8bcb6944df5cb3
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-28 12:14:04 +0530

    area

--- m1/Assignment-3/Assignment-2/Solution.java
+++ m1/Assignment-3/Assignment-2/Solution.java
@@ -1,22 +1,42 @@
+/**.
+    * Author : praneetha.
+    * date : 27/08/18
+*/
 import java.util.Scanner;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		float res;
-		int base = scan.nextInt();
-		int height = scan.nextInt();
-		res = areaOfTriangle(base, height);
-		System.out.println(res);
-	}
-	/*
-	Need to fill the areaOfTriangle function and print the output
-	of fahrenheit.
-	*/
-	public static int areaOfTriangle(int b, int h) {
-		return (b *h)/2;
-	}
+/**.
+    * scanner function for taking user inputs
+*/
+public final class Solution {
+    /**.
+    Do not modify this main function.
+    */
+    private Solution() {
+        //constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float res;
+        int base = scan.nextInt();
+        int height = scan.nextInt();
+        res = areaOfTriangle(base, height);
+        System.out.println(res);
+    }
+    /**
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    *@return float
+    /**
+    * this is comment of function
+    * @param b **this is base of trianle**
+    * @param h **this is height of triangle**
+    */
+    public static int areaOfTriangle(final int b, final int h) {
+    /**.
+    function
+    */
+        return (b * h) / 2;
+    }
 }
