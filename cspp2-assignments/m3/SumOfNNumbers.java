0c4c415c171a85288a46d3d6850ac60355b910c1
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-29 10:38:45 +0530

    sum

---
+++ cspp2-assignments/m3/SumOfNNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumOfNNumbers {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int i = 1;
+		while(i <= n ) {
+			sum += i;
+			i = i + 1; 
+		}
+		System.out.println(+sum);	
+	}
+}
