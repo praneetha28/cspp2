0a00e78b13c8658d6d71f86491246131658a9a39
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-30 13:55:16 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -0,0 +1,35 @@
+/**.
+    * Author : praneetha.
+    * date : 29/08/18
+*/
+import java.util.Scanner;
+/**.
+    * scanner function for taking user input
+*/
+public final class Solution {
+    /**.
+     Fill the main function to print the number of 7's between 1 to n
+    */
+    private Solution() {
+        //constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+	public static void main(final String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt(); 
+		}
+		int max = arr[0];
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+	}
+}
