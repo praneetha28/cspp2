983d90401f679b97c34b8a80559f505be90961aa
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-30 15:11:08 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -43,8 +43,7 @@ public final class Solution {
         	}
         	for (int i = 0; i < r; i++) {
         		for (int j = 0; j < c1; j++) {
-        			System.out.print(res[i][j]);
-        			System.out.print(" ");
+        			System.out.print(res[i][j]+" ");
         		}
         		System.out.println();
         	}
