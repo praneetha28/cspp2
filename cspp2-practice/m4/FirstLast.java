28476da82f8aee88002c38e183bb70caa130a3c4
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-30 15:59:23 +0530

    first

--- cspp2-practice/m4/FirstLast.java
+++ cspp2-practice/m4/FirstLast.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class FirstLast {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i=0; i<n; i++) {
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for(int i=0; i<size; i++) {
 			arr[i] = sc.nextInt();
 		}
 		if (arr[0] == 6 || arr[arr.length-1] == 6) {
