dac97540d5be30eac93677798c19a0c866d27de7
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-30 14:04:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -1,6 +1,6 @@
 /**.
     * Author : praneetha.
-    * date : 29/08/18
+    * date : 30/08/18
 */
 import java.util.Scanner;
 /**.
@@ -16,20 +16,19 @@ public final class Solution {
     /**.
         * @param args of type string : command line arguments
     */
-	public static void main(final String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt(); 
-		}
-		int max = arr[0];
-		for (int i = 0; i < n; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-			}
-		}
-		System.out.println(max);
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
