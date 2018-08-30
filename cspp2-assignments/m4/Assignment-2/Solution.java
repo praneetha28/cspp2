e195947184362d74322fd4fae700e974cb7d2d6a
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-30 15:23:48 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -16,37 +16,37 @@ public final class Solution {
     /**.
         * @param args of type string : command line arguments
     */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int r = sc.nextInt();
-		int c = sc.nextInt();
-		int[][] arr1 = new int[r][c];
-		for (int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-            	arr1[i][j] = sc.nextInt();
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr1 = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr1[i][j] = sc.nextInt();
             }
         }
         int r1 = sc.nextInt();
-		int c1 = sc.nextInt();
-		int[][] arr2 = new int[r1][c1];
-		for (int i = 0; i < r1; i++) {
-            for(int j = 0; j < c1; j++) {
-            	arr2[i][j] = sc.nextInt();
+        int c1 = sc.nextInt();
+        int[][] arr2 = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                arr2[i][j] = sc.nextInt();
             }
         }
         if (r == r1 && c == c1) {
-        	String res1 = "";
-        	int[][] res = new int[r1][c1];
-        	for (int i = 0; i < r; i++) {
-        		for (int j = 0; j < c1; j++) {
-        			res[i][j] = arr1[i][j] + arr2[i][j];
-        			res1 += res[i][j] + " ";
-        		}
-        		res1 = res1.trim() + "\n";
-        	}
-        	System.out.println(res1);
+            String res1 = "";
+            int[][] res = new int[r1][c1];
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c1; j++) {
+                    res[i][j] = arr1[i][j] + arr2[i][j];
+                    res1 += res[i][j] + " ";
+                }
+                res1 = res1.trim() + "\n";
+            }
+            System.out.println(res1);
         } else {
-        	System.out.println("not possible");
+            System.out.println("not possible");
         }
-	}
+    }
 }
