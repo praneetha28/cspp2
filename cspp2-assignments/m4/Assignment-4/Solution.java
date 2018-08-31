b130a6b7d0c47e6bf705146d94b14514d25e34e2
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-31 15:35:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -0,0 +1,42 @@
+/**.
+    * Author : praneetha.
+    * date : 31/08/18
+*/
+import java.util.Scanner;
+/**.
+    * scanner function for taking user input
+*/
+public final class Solution {
+    /**.
+     Fill the main function to reverse the string
+    */
+    private Solution() {
+        //constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+	public static void main(final String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String reverse = reverseString(s);	
+		System.out.println(reverse);
+		
+	}
+	/**.
+    Need to write the function for conversion
+    *@return string
+    /**
+    * this is comment of function
+    * @param str **this is word**
+    */
+	public static String reverseString(final String str) {
+		String rev = "";
+		for (int i = 0; i < str.length(); i++) {
+			rev = str.charAt(i) + rev;
+		}
+		return rev;
+	}
+
+}
