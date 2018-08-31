a02834dc92fdac6e5fde679517efa79e896cefce
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-31 15:23:59 +0530

    "check_style_score -> 0 of 1 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2-assignments/m4/Assignment-3/Solution.java
@@ -1,30 +1,46 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
+/**.
+    * Author : praneetha.
+    * date : 31/08/18
+*/
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=0;i<=n-1;i++) {
-			String s=sc.next();
-			int res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static int binaryToDecimal(String str) {
-		int decimal = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == '1') {
-				decimal += java.lang.Math.pow(2, str.length()-1-i); 
-			}
-		}
-		return decimal;
-	}
+/**.
+    * scanner function for taking user input
+*/
+public final class Solution {
+    /**.
+     Fill the main function to convert binary to decimal
+    */
+    private Solution() {
+        //constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n - 1; i++) {
+            sc.nextLine();
+            String s = sc.nextLine();
+            int res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**.
+    Need to write the function for conversion
+    *@return int
+    /**
+    * this is comment of function
+    * @param str **this is binary number**
+    */
+    public static int binaryToDecimal(final String str) {
+        int decimal = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                decimal += Math.pow(2, str.length() - 1 - i);
+            }
+        }
+        return decimal;
+    }
 
 }
