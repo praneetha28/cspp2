385be54ff821caed7f21faa6db40d71891227acc
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-08-31 15:37:01 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -16,27 +16,25 @@ public final class Solution {
     /**.
         * @param args of type string : command line arguments
     */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	/**.
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**.
     Need to write the function for conversion
     *@return string
     /**
     * this is comment of function
     * @param str **this is word**
     */
-	public static String reverseString(final String str) {
-		String rev = "";
-		for (int i = 0; i < str.length(); i++) {
-			rev = str.charAt(i) + rev;
-		}
-		return rev;
-	}
+    public static String reverseString(final String str) {
+        String rev = "";
+        for (int i = 0; i < str.length(); i++) {
+            rev = str.charAt(i) + rev;
+        }
+        return rev;
+    }
 
 }
