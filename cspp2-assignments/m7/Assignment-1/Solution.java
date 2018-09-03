be087569c43c8781717b1f9652d2f7eefc7ee0d0
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-03 18:39:10 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -7,25 +7,50 @@ import java.util.Scanner;
     * scanner function for taking user input
 */
 class InputValidator {
-    /*Write the atrributes and methods for InputValidator*/
-    String data;
-    int length;
-    InputValidator(String data, int len) {
-        this.data = data;
+    /**.
+     *Write the atrributes and methods for InputValidator
+    */
+    private String data;
+    /**.
+     attributes
+    */
+    private int length;
+    /**.
+        constructor
+    */
+    /**.
+    * this is comment of function
+    * @param str **data**
+    * @param len **len for data**
+    */
+    InputValidator(final String str, final int len) {
+        this.data = str;
         this.length = len;
     }
+    /**.
+        method for comapring
+    */
     public static final int N_U_M = 6;
     /**.
         this is to compare
     */
-    public static boolean validateData(int length) {
+    /**.
+    Need to write function for validating.
+    *@return boolean
+    /**
+    * this is comment of function
+    * @param length **len for data**
+    */
+    public static boolean validateData(final int length) {
         if (length >= N_U_M) {
             return true;
         }
         return false;
     }
-
 }
+/**.
+    main class
+*/
 public final class Solution {
     /**.
      Fill the main function to print the number of 7's between 1 to n
