7f6f70b12f1044cbc3aecb7aaab6c069641ea81e
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-03 18:18:18 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,32 +1,46 @@
-import java.util.*;
-class InputValidator
-{
-	/*Write the atrributes and methods for InputValidator*/
-	String data;
-	int length;
-	InputValidator(String data, int len) {
-		this.data = data;
-		this.length = len;
-	}
-	public static boolean validateData(int length) {
-		if(length >= 6) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-}
-public class Solution
-{
-	public static void main(String args[])
-    {
-    	Scanner s=new Scanner(System.in);
-    	String input=s.next();
-    	int length = input.length();
-    	InputValidator i=new InputValidator(input, length);    	
-    	System.out.println(i.validateData(length));
-
+/**.
+    * Author : praneetha.
+    * date : 3/09/18
+*/
+import java.util.Scanner;
+/**.
+    * scanner function for taking user input
+*/
+class InputValidator {
+    /*Write the atrributes and methods for InputValidator*/
+    String data;
+    int length;
+    InputValidator(String data, int len) {
+        this.data = data;
+        this.length = len;
+    }
+    public static final int N_U_M = 6;
+    /**.
+        this is to compare
+    */
+    public static boolean validateData(int length) {
+        if (length >= N_U_M) {
+            return true;
+        }
+        return false;
     }
 
+}
+public final class Solution {
+    /**.
+     Fill the main function to print the number of 7's between 1 to n
+    */
+    private Solution() {
+        //constructor
+    }
+    /**.
+        * @param args of type string : command line arguments
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        int length = input.length();
+        InputValidator i = new InputValidator(input, length);
+        System.out.println(i.validateData(length));
+    }
 }
