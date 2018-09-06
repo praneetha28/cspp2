fa3d83a3d6f5a5d1d86741a8a712f04092243a5f
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-06 18:17:32 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-3/Solution.java
+++ cspp2-assignments/m10/Assignment-3/Solution.java
@@ -84,7 +84,6 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        // create an object of the list to invoke methods on it
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         String objectType = stdin.nextLine();
         switch (objectType) {
@@ -362,64 +361,56 @@ public final class Solution {
                 }
             break;
 
-            case "O"://This will be executed for Student type list
-             //i.e to store List of Student Objects
-                List<Student> listStudent = new List();
-                while (stdin.hasNext()) {
-                    // read the line
-                    String line = stdin.nextLine();
-                    // split the line using space
-                    String[] tokens = line.split(" ");
-                    // based on the list op invoke the corresponding method
-                    switch (tokens[0]) {
-                        case "add":
-                            listStudent.add(new Student(tokens[1]));
-                            break;
-                        case "addAll":
-                            if (tokens.length == 2) {
-                                String[] t1 = tokens[1].split(",");
-                                Student[] temp = new Student[t1.length];
-                                for (int i = 0; i < t1.length; i++) {
-                                    temp[i] = new Student(t1[i]);
-                                }
-                                listStudent.addAll(temp);
-                            }
-                            break;
-                        case "size":
-                        // invoke size method and print the list size
-                        // BTW, list size is not the array size
-                        // it is the number of items in the list
-                            System.out.println(listStudent.size());
-                            break;
-                        case "print":
-                        // print the list (implement toString in List class
-                        //for this to work)
-                        // expected format is [item-1,item-2,...,item-n]
-                        // review the output testcase file
-                            System.out.println(listStudent);
-                            break;
-                        case "remove":
-                            listStudent.remove(Integer.parseInt(tokens[1]));
-                            break;
-                        case "indexOf":
-                            System.out.println(listStudent.
-                                indexOf(new Student(tokens[1])));
-                            break;
-                        case "get":
-                            System.out.println(listStudent.
-                                get(Integer.parseInt(tokens[1])));
-                            break;
-                        case "contains":
-                            System.out.println(listStudent.
-                                contains(new Student(tokens[1])));
-                            break;
-                        default:
-                            break;
-                    }
-                }
+            case "O":
+                performDouble(stdin);
                 break;
             default:
             break;
+        }
+    }
+    public static void performDouble(final Scanner stdin) {
+        List<Student> listStudent = new List();
+        while (stdin.hasNext()) {
+            String line = stdin.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case "add":
+                    listStudent.add(new Student(tokens[1]));
+                    break;
+                case "addAll":
+                    if (tokens.length == 2) {
+                        String[] t1 = tokens[1].split(",");
+                        Student[] temp = new Student[t1.length];
+                        for (int i = 0; i < t1.length; i++) {
+                            temp[i] = new Student(t1[i]);
+                        }
+                        listStudent.addAll(temp);
+                    }
+                    break;
+                case "size":
+                    System.out.println(listStudent.size());
+                    break;
+                case "print":
+                    System.out.println(listStudent);
+                    break;
+                case "remove":
+                    listStudent.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "indexOf":
+                    System.out.println(listStudent.
+                    indexOf(new Student(tokens[1])));
+                    break;
+                case "get":
+                    System.out.println(listStudent.
+                    get(Integer.parseInt(tokens[1])));
+                    break;
+                case "contains":
+                    System.out.println(listStudent.
+                    contains(new Student(tokens[1])));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
