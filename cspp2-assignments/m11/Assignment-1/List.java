3dea1650d86d3dc6c2af1da2dcce3ad9cab5a2ab
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-07 11:56:14 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2-assignments/m11/Assignment-1/List.java
@@ -365,7 +365,7 @@ public class List {
     	}
     	for (int i = 0; i < size; i++) {
     		if (list.contains(this.list[i])) {
-    			return true;
+    			return false;
     		}
     	}
     	return true;
