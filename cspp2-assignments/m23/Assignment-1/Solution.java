d4acea74ca51799601f7297dc034f427c243e602
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-21 09:00:49 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -1,10 +1,32 @@
-import java.util.*;
+/**.
+ * { item_description }
+ */
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
 import java.io.File;
+/**.
+ * { item_description }
+ */
 public final class Solution {
+	/**.
+	 * Constructs the object.
+	 */
 	private Solution() {
-
+		/**.
+		 * { item_description }
+		 */
 	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
+		/**.
+		 * { var_description }
+		 */
 		Plagiarism plg = new Plagiarism();
 		Scanner scan = new Scanner(System.in);
 		try {
@@ -16,7 +38,8 @@ public final class Solution {
 			for(int i = 0;i < l; i++) {
 				for (int j = 0; j < l; j++) {
 					if (i < j) {
-						matrix[i][j] = plg.bagOfWords(plg.toString(listFiles[i]), plg.toString(listFiles[j]));
+						matrix[i][j] = plg.bagOfWords(plg.toString(
+							listFiles[i]), plg.toString(listFiles[j]));
 					} else {
 						matrix[i][j] = matrix[j][i];
 					}
